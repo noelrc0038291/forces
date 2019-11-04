c46f3b9412dd9056ba8509ed40ae7d0ea32048a7
Noel Holmes <rc0038291@student.riversidecollege.ac.uk>
2019-11-04 15:21:52 +0000

    asdfasdfadf

--- forces/Form1.cs
+++ forces/Form1.cs
@@ -25,6 +25,12 @@ namespace forces
         {
             return (Math.Asin(x) * 180 / Math.PI); 
         }
+
+        double cos(double x)
+        {
+            return (Math.Cos(x * Math.PI / 180.0));
+        }
+               
         private void PictureBox1_Click(object sender, EventArgs e)
         {
 
@@ -32,6 +38,36 @@ namespace forces
 
         private void TextBox1_TextChanged(object sender, EventArgs e)
         {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            double force, angle;
+            try
+            {
+                force = double.Parse(textBox1.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Incorrect value for force.");
+                force = 0.0;
+            }
+            try
+            {
+                angle = double.Parse(textBox2.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Incorrect value for angle.");
+                angle = 0.0;
+            }
+
+            double Fx = force * cos(angle);
+            label1.Text = "Fx = " + Fx + " N";
+
+            double Fy = force * sin(angle);
+            label2.Text = "Fy = " + Fy + " N";
 
         }
     }
